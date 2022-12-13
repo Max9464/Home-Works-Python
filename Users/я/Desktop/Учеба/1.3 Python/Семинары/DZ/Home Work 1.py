8c1edd122f626539379ad422ad045af1714563e2
Kinder <kindermaks@mail.ru>
2022-12-14 02:23:29 +0300

    dz1

---
+++ Users/я/Desktop/Учеба/1.3 Python/Семинары/DZ/Home Work 1.py
@@ -0,0 +1,23 @@
+# Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+# Пример:
+
+# - 6 -> да
+# - 7 -> да
+# - 1 -> нет
+
+week_day = int(input('Input day of week: '))
+
+if week_day == 1:
+    print('Нет')
+elif week_day == 2:
+    print('Нет')
+elif week_day == 3:
+    print('Нет')
+elif week_day == 4:
+    print('Нет')
+elif week_day == 5:
+    print('Нет')
+else:
+    print('Да')
+
