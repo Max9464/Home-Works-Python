35f7d1338e1130cf882e5fdef7429c5ab3edffb0
Kinder <kindermaks@mail.ru>
2022-12-19 19:06:41 +0300

    final

--- Users/я/Desktop/Учеба/1.3 Python/Семинары/SEM2/Home Work 2.py
+++ Users/я/Desktop/Учеба/1.3 Python/Семинары/SEM2/Home Work 2.py
@@ -3,12 +3,13 @@
 # 6782 -> 23
 # 0,56 -> 11
 
-number = int(input('Введите число: '))
-sum = 0
-while number != 0:
-    sum = (number % 10) + sum
-    number //= 10
-print(sum)
+# num = input('Введите число: ')
+# sum = 0
+# for a in num:
+#     if a.isdigit():
+#         sum += int(a)
+#
+# print(f"Сумма {num} равна: ", sum)
 
 # 2.Задайте список из n чисел последовательности (1 + 1/n)^n, выведеите его на экран, а так же сумму элементов списка.
 # Пример:
@@ -24,10 +25,12 @@ print(sum)
 # 3.Реализуйте алгоритм перемешивания списка. НЕ ИСПОЛЬЗОВАТЬ ВСТРОЕННЫЕ БИБЛИОТЕКИ SHUFFLE,
 # максимум использование библиотеки Random для и получения случайного int
 
-# nums =['apple','orange','bannano','mandaryn','avokado']
-# for num in nums:
-#     print(num)
-# print()
-
+import random
+list = [0,1,2,3,4,5,6,7,8,9]
+print ("Начальный список: " + str(list))
+for i in range(len(list)-1, 0, -1):
+    j = random.randint(0, i + 1)
+    list[i], list[j] = list[j], list[i]
+print ("Перемешанный список: " +  str(list))
 
 
