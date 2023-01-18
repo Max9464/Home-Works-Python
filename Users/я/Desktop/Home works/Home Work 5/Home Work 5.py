86ec9ebe1ad5e3fe7d9a963d406a1fd21fa34e0a
Kinder <kindermaks@mail.ru>
2023-01-19 02:07:17 +0300

    game whith bot

--- Users/я/Desktop/Home works/Home Work 5/Home Work 5.py
+++ Users/я/Desktop/Home works/Home Work 5/Home Work 5.py
@@ -18,6 +18,7 @@ start_number = randint(100, 300)
 print('\n' + ho_is_first.upper() + ' is First!')
 print("This is a candy number's: " + str(start_number) + " candy's")
 
+# Cod if bot going first
 if ho_is_first == bot:
     bot_hod = randint(1, 28)
     print('CANDY KILLER: -'+str(bot_hod))
@@ -27,6 +28,7 @@ if ho_is_first == bot:
     print('\nNext Player: ' + name_player.upper())
 
     while True:
+# Player_2 hod
         player_2 = int(input(name_player.upper() + ' Take you candy from 1 to 28: '))
         while player_2 > 28:
             print('--------------- ERROR! ---------------- ')
@@ -40,11 +42,20 @@ if ho_is_first == bot:
             break
         print('\n----------------<< ' + str(count) + ' >>----------------')
         print('\nNext Player: ' + bot)
-
-        bot_hod = randint(1, 28)
+# Bot hod
+        if count == 56:
+            bot_hod = 27
+            count = start_number - bot_hod
+        elif count <= 28:
+            bot_hod = 0
+            count = start_number * bot_hod
+            start_number = count
+        else:
+            bot_hod = randint(1, 28)
         print('CANDY KILLER: -' + str(bot_hod))
         count = start_number - bot_hod
         start_number = count
+# Cheking Winner
         if count <= 0:
             print('')
             print('              ' + str(bot))
@@ -53,6 +64,7 @@ if ho_is_first == bot:
         print('\n----------------<< ' + str(count) + ' >>----------------')
         print('\nNext Player: ' + name_player.upper())
 
+# Cod if Second player "name_player" going first
 else:
     player_2 = int(input(name_player.upper() + ' Take you candy from 1 to 28: '))
     while player_2 > 28:
@@ -64,10 +76,20 @@ else:
     print('\nNext Player: ' + bot)
 
     while True:
-        bot_hod = randint(1, 28)
+# Bot hod
+        if count == 56:
+            bot_hod = 27
+            count = start_number - bot_hod
+        elif count <= 28:
+            bot_hod = 0
+            count = start_number * bot_hod
+            start_number = count
+        else:
+            bot_hod = randint(1, 28)
         print('CANDY KILLER: -' + str(bot_hod))
         count = start_number - bot_hod
         start_number = count
+# Cheking Winner
         if count <= 0:
             print('')
             print('              ' + str(bot))
@@ -75,7 +97,7 @@ else:
             break
         print('\n----------------<< ' + str(count) + ' >>----------------')
         print('\nNext Player: ' + name_player.upper())
-
+# Player_2 hod
         player_2 = int(input(name_player.upper() + ' Take you candy from 1 to 28: '))
         while player_2 > 28:
             print('--------------- ERROR! ---------------- ')
@@ -89,3 +111,7 @@ else:
             break
         print('\n----------------<< ' + str(count) + ' >>----------------')
         print('\nNext Player: ' + bot)
+
+
+
+
